d8d53f9ddda83d54a2381ae8085d7ae52efbd2b2
smudgil <38195323+smudgil@users.noreply.github.com>
2021-04-22 17:35:58 -0400

    Update interact.js

--- nft-minter/src/util/interact.js
+++ nft-minter/src/util/interact.js
@@ -61,7 +61,7 @@ export const mintNFT = async(url, name, description) => {
     } 
     const tokenURI = pinataResponse.pinataUrl;  
 
-    window.contract = await new web3.eth.Contract(contractABI, contractAddress);//loadContract();
+    window.contract = await new web3.eth.Contract(contractABI, contractAddress);
 
     const transactionParameters = {
         to: contractAddress, // Required except during contract publications.

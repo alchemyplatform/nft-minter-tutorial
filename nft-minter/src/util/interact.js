8067253569695428f0f2a4dae2d26df79c46a7cc
Rossi Meacham <rossimeacham@gmail.com>
2023-01-02 12:54:59 -0500

    fix security risk by adding rel noreferrer to target blank links

--- nft-minter/src/util/interact.js
+++ nft-minter/src/util/interact.js
@@ -31,7 +31,11 @@ export const connectWallet = async () => {
           <p>
             {" "}
             🦊{" "}
-            <a target="_blank" href={`https://metamask.io/download.html`}>
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href={`https://metamask.io/download.html`}
+            >
               You must install Metamask, a virtual Ethereum wallet, in your
               browser.
             </a>
@@ -73,7 +77,11 @@ export const getCurrentWalletConnected = async () => {
           <p>
             {" "}
             🦊{" "}
-            <a target="_blank" href={`https://metamask.io/download.html`}>
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href={`https://metamask.io/download.html`}
+            >
               You must install Metamask, a virtual Ethereum wallet, in your
               browser.
             </a>
